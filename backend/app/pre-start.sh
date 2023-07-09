bc681a43be219ddecb35e3dfc6f00975591f0d40
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-07-09 14:41:59 +0200

    CMS-3: Add a pre-start script and update Docker setup

---
+++ backend/app/pre-start.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+# Let the DB start
+python -m app.backend_pre_start
+
+# Run migrations
+alembic upgrade head
+
+# Create initial data in DB
+# python -m app.initial_data
+
+# Start the FastAPI app
+python -m app.main
---
+++ backend/start.sh
@@ -0,0 +1,12 @@
+#! /usr/bin/env sh
+set -e
+
+# If there's a pre-start.sh script in the specified directory, run it before starting
+PRE_START_PATH=app/pre-start.sh
+echo "Checking for script in $PRE_START_PATH"
+if [ -f $PRE_START_PATH ] ; then
+    echo "Running script $PRE_START_PATH"
+    pipenv run "$PRE_START_PATH"
+else
+    echo "There is no script $PRE_START_PATH"
+fi
