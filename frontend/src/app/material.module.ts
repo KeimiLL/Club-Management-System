6fd2d0eb4c8813df45f602b2d68e164b04e56ee7
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-07-27 17:23:01 +0200

    CMS-33: change footer style

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @NgModule({
-    exports: [MatButtonModule],
+    exports: [MatButtonModule, MatToolbarModule],
 })
 export class MaterialModule {}
