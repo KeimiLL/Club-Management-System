6001365ab442f2109a18e7fb86e9f8daa72e5b19
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-07-30 23:42:25 +0200

    CMS-34: standalone components

--- frontend/src/app/components/routes/main-view/components/menu/components/menu-header/menu-header.component.ts
+++ frontend/src/app/components/routes/main-view/components/menu/components/menu-header/menu-header.component.ts
@@ -1,9 +1,13 @@
+import { CommonModule } from "@angular/common";
 import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { MaterialModule } from "src/app/shared/modules/material.module";
 
 @Component({
     selector: "app-menu-header",
     templateUrl: "./menu-header.component.html",
     styleUrls: ["./menu-header.component.scss"],
+    standalone: true,
+    imports: [CommonModule, MaterialModule],
 })
 export class MenuHeaderComponent {
     @Input() public isCollapsed: boolean;
--- frontend/src/app/components/routes/main-view/components/menu/components/menu-item/menu-item.component.ts
+++ frontend/src/app/components/routes/main-view/components/menu/components/menu-item/menu-item.component.ts
@@ -1,10 +1,15 @@
 import { Component, Input } from "@angular/core";
 import { MenuItem } from "../../menu.component";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { MaterialModule } from "src/app/shared/modules/material.module";
 
 @Component({
     selector: "app-menu-item",
     templateUrl: "./menu-item.component.html",
     styleUrls: ["./menu-item.component.scss"],
+    standalone: true,
+    imports: [CommonModule, RouterModule, MaterialModule],
 })
 export class MenuItemComponent {
     @Input() item: MenuItem;
