ab7d443c31c3bfb472ce85ff3f68630785be779f
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-07-30 23:45:04 +0200

    CMS-34: add routing, routes, and connect routes with menu

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { MainViewComponent } from "./components/routes/main-view/main-view.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: "app",
+        component: MainViewComponent,
+        loadChildren: () =>
+            import(
+                "./components/routes/main-view/main-view-routing.module"
+            ).then((m) => m.MainViewRoutingModule),
+    },
+    { path: "**", redirectTo: "app", pathMatch: "full" },
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
