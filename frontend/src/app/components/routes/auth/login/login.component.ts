7171b86ad2494130596c97b5cbd8acd46c133495
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-08-03 00:19:45 +0200

    CMS-36: Add Login view backbone and refactor register view

--- frontend/src/app/components/routes/auth/login/login.component.ts
+++ frontend/src/app/components/routes/auth/login/login.component.ts
@@ -1,8 +1,33 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Validators, FormBuilder, FormGroup } from "@angular/forms";
 
 @Component({
     selector: "app-login",
     templateUrl: "./login.component.html",
     styleUrls: ["./login.component.scss"],
 })
-export class LoginComponent {}
+// export class LoginComponent implements OnInit {
+export class LoginComponent {
+    loginForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    // ngOnInit(): void {
+
+    // }
+
+    // onSubmit() {
+
+    // }
+
+    passwordMatchValidator(formGroup: FormGroup) {
+        const passwordControl = formGroup.get("password");
+        const confirmPasswordControl = formGroup.get("confirmPassword");
+
+        if (passwordControl?.value !== confirmPasswordControl?.value) {
+            confirmPasswordControl?.setErrors({ passwordMismatch: true });
+        } else {
+            confirmPasswordControl?.setErrors(null);
+        }
+    }
+}
