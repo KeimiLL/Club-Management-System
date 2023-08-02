7ea1804beb66138780cdbd5e2e67ac8e600272b6
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-08-02 23:47:39 +0200

    CMS-36: Fix imports

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,12 @@
-import "./app-routing.module"
-import "./app.component"
-import "./components/base/footer/footer.component"
-import "./components/routes/auth/auth.component"
-import "./components/routes/auth/modules/auth.module"
-import "@angular/common/http"
-import "@angular/core"
-import "@angular/platform-browser"
-import "@angular/platform-browser/animations"
-import { AppComponent }
-import { AppRoutingModule }
-import { AuthComponent }
-import { AuthModule }
-import { BrowserAnimationsModule }
-import { BrowserModule }
-import { FooterComponent }
-import { HttpClientModule }
-import { NgModule }
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./components/base/footer/footer.component";
+import { AuthComponent } from "./components/routes/auth/auth.component";
+import { AuthModule } from "./components/routes/auth/modules/auth.module";
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 // import { MainViewComponent } from "./components/routes/main-view/main-view.component";
 // import { FooterModule } from "./components/base/footer/footer.module";
