5768096c1e433440447c049b4838a591dbf17afa
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-08-02 23:24:42 +0200

    CMS-38: add basic settings menu without connection

--- frontend/src/app/components/routes/main-view/components/settings/settings.component.ts
+++ frontend/src/app/components/routes/main-view/components/settings/settings.component.ts
@@ -2,12 +2,11 @@ import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
 import { SettingsMenuComponent } from "./components/settings-menu/settings-menu.component";
-import { RouterModule } from "@angular/router";
 
 @Component({
     selector: "app-settings",
     standalone: true,
-    imports: [CommonModule, RouterModule, SettingsMenuComponent, RouterModule],
+    imports: [CommonModule, RouterModule, SettingsMenuComponent],
     templateUrl: "./settings.component.html",
     styleUrls: ["./settings.component.scss"],
 })
