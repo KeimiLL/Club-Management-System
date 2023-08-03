151a4905d8127c6cd7c065f3fc3b2afe729afbed
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-08-04 01:09:51 +0200

    CMS-36: fix routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,29 +1,24 @@
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { FooterComponent } from "./components/base/footer/footer.component";
-import { AuthComponent } from "./components/routes/auth/auth.component";
-import { AuthModule } from "./components/routes/auth/modules/auth.module";
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-
-// import { MainViewComponent } from "./components/routes/main-view/main-view.component";
-// import { FooterModule } from "./components/base/footer/footer.module";
-// import { MainViewRoutingModule } from "./components/routes/main-view/main-view-routing.module";
+import { MainViewRoutingModule } from "./components/routes/main-view/main-view-routing.module";
+import { AuthRoutingModule } from "./components/routes/auth/auth-routing.module";
 
 @NgModule({
-    declarations: [AppComponent, AuthComponent],
+    declarations: [AppComponent],
     imports: [
         BrowserModule,
         AppRoutingModule,
         BrowserAnimationsModule,
         HttpClientModule,
         FooterComponent,
-        // MainViewComponent,
-        // FooterModule,
-        // MainViewRoutingModule,
-        AuthModule,
+        FooterComponent,
+        MainViewRoutingModule,
+        AuthRoutingModule,
     ],
     providers: [],
     bootstrap: [AppComponent],
--- frontend/src/app/shared/modules/material.module.ts
+++ frontend/src/app/shared/modules/material.module.ts
@@ -3,8 +3,16 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
 import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
 
 @NgModule({
-    exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
+    exports: [
+        MatButtonModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatCardModule,
+        MatInputModule,
+        MatCardModule,
+    ],
 })
 export class MaterialModule {}
