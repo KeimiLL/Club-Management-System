dbc0a573f40f87295773e9b9378029a560e706a7
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-08-04 18:23:36 +0200

    CMS-43: add half-card-compoennt and connect it to meetings

---
+++ frontend/src/app/shared/components/half-card/half-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+    selector: "app-half-card",
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: "./half-card.component.html",
+    styleUrls: ["./half-card.component.scss"],
+})
+export class HalfCardComponent {}
