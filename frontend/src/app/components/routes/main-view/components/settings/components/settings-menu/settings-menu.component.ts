46a711bb298be46592ca136e24333ff2e606fbdb
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-08-04 15:24:23 +0200

    CMS-38: add settings menu links generator and style them

--- frontend/src/app/components/routes/main-view/components/settings/components/settings-menu/settings-menu.component.ts
+++ frontend/src/app/components/routes/main-view/components/settings/components/settings-menu/settings-menu.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { settingsMenuItems } from "./settings-menu.data";
+import { RouterModule } from "@angular/router";
 
 export interface SettingsMenuItem {
     name: string;
@@ -10,7 +11,7 @@ export interface SettingsMenuItem {
 @Component({
     selector: "app-settings-menu",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, RouterModule],
     templateUrl: "./settings-menu.component.html",
     styleUrls: ["./settings-menu.component.scss"],
 })
