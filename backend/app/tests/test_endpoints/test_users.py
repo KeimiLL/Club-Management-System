afee2151beed68087fc6c2795709581f4b64613b
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-08-04 23:02:58 +0200

    CMS-20: Add tests for users/current endpoint

--- backend/app/tests/test_endpoints/test_users.py
+++ backend/app/tests/test_endpoints/test_users.py
@@ -106,3 +106,49 @@ def test_incorrect__logout(client: TestClient) -> None:
 
     assert response.status_code == 401
     assert response.json()["message"] == "Invalid tokens"
+
+
+def test_correct__get_current_user(
+    client: TestClient,
+    correct_user_data: dict[str, str],
+) -> None:
+    """Tests getting the currently logged in user.
+
+    Args:
+        client (TestClient): TestClient instance.
+        correct_user_data (dict[str, str]): Data to be sent.
+    """
+    client.post(
+        "/api/v1/users/login",
+        json={
+            "email": correct_user_data["email"],
+            "password": correct_user_data["password"],
+        },
+    )
+    response = client.get(
+        "/api/v1/users/current",
+    )
+
+    assert response.status_code == 200
+    assert response.json()["full_name"] == correct_user_data["full_name"]
+    assert response.json()["email"] == correct_user_data["email"]
+    assert response.json()["role"] == correct_user_data["role"]
+
+
+def test_incorrect__get_current_user(
+    client: TestClient,
+) -> None:
+    """Tests getting the currently logged in when there is none.
+
+    Args:
+        client (TestClient): TestClient instance.
+    """
+    client.post(
+        "/api/v1/users/logout",
+    )
+    response = client.get(
+        "/api/v1/users/current",
+    )
+
+    assert response.status_code == 401
+    assert response.json()["message"] == "Invalid tokens"
