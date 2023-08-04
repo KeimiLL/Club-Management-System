bae4a9406149fc27ec9c92be138cb495f9a0256e
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-08-04 15:59:40 +0200

    CMS-42: fix module and add it meetings

--- frontend/src/app/shared/modules/cards.module.ts
+++ frontend/src/app/shared/modules/cards.module.ts
@@ -1,12 +1,10 @@
 import { WideMainCardComponent } from "./../components/wide-main-card/wide-main-card.component";
 import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
 import { ButtonContainerComponent } from "../components/button-container/button-container.component";
 
 @NgModule({
-    declarations: [],
-    imports: [
-        CommonModule,
+    imports: [WideMainCardComponent, ButtonContainerComponent],
+    exports: [
         WideMainCardComponent,
         ButtonContainerComponent,
         //in future Half-card-component
