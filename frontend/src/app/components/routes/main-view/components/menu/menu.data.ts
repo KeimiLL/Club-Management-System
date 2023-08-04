4963161d4babb5a0a1802087def56425251a464e
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-08-04 15:44:44 +0200

    CMS-42: connect meetings to the routing

--- frontend/src/app/components/routes/main-view/components/menu/menu.data.ts
+++ frontend/src/app/components/routes/main-view/components/menu/menu.data.ts
@@ -2,5 +2,6 @@ import { MenuItem } from "./menu.component";
 
 export const menuItems: MenuItem[] = [
     { name: "dashboard", icon: "", route: "/app/dashboard" },
+    { name: "meetings", icon: "", route: "/app/meetings" },
     { name: "settings", icon: "", route: "/app/settings" },
 ];
--- frontend/src/app/components/routes/main-view/main-view-routing.module.ts
+++ frontend/src/app/components/routes/main-view/main-view-routing.module.ts
@@ -2,11 +2,13 @@ import { SettingsComponent } from "./components/settings/settings.component";
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { MeetingsComponent } from "./components/meetings/meetings.component";
 
 const routes: Routes = [
     { path: "", redirectTo: "dashboard", pathMatch: "full" },
     { path: "dashboard", component: DashboardComponent },
     { path: "settings", component: SettingsComponent },
+    { path: "meetings", component: MeetingsComponent },
 ];
 
 @NgModule({
