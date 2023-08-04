7b07e6558c48fa7deb008bd520a20c1cf386c354
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-08-04 21:22:45 +0200

    CMS-20: Use access and refresh tokens in authentication flow

--- backend/app/api/v1/endpoints/users.py
+++ backend/app/api/v1/endpoints/users.py
@@ -3,16 +3,19 @@
 
 from typing import Annotated
 
+from fastapi import APIRouter, Depends, Response, status
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+
+from app.api.dependencies import get_user_from_token, refresh_token_dependency
 from app.core.exceptions import InvalidCredentialsException
+from app.core.jwt_utils import create_access_token, create_refresh_token
 from app.core.security import Hasher
 from app.crud.crud_user import create_new_user, get_user_by_email
 from app.db.session import get_db
 from app.schemas.enums import ExceptionMessages
 from app.schemas.misc import Message, MessageFromEnum
 from app.schemas.user import User, UserCreate, UserLogin
-from fastapi import APIRouter, Depends, status
-from fastapi.responses import JSONResponse
-from sqlalchemy.orm import Session
 
 router = APIRouter()
 
@@ -47,10 +50,15 @@ def register(user: UserCreate, db: Annotated[Session, Depends(get_db)]):
         status.HTTP_409_CONFLICT: {"model": MessageFromEnum},
     },
 )
-def login(user: UserLogin, db: Annotated[Session, Depends(get_db)]):
+def login(
+    response: Response,
+    user: UserLogin,
+    db: Annotated[Session, Depends(get_db)],
+):
     """Logs the user in if the provided credentials are valid.
 
     Args:
+        response (Response): Response object.
         user (UserLogin): User data from a POST request.
         db (Annotated[Session, Depends]): Database session. Defaults to Depends(get_db).
 
@@ -60,17 +68,67 @@ def login(user: UserLogin, db: Annotated[Session, Depends(get_db)]):
     Returns:
         user_by_email (User): The logged in user.
     """
+    access_token = create_access_token({"sub": user.email})
+    refresh_token = create_refresh_token({"sub": user.email})
+
+    response.set_cookie(key="access_token", value=access_token, httponly=True)
+    response.set_cookie(key="refresh_token", value=refresh_token, httponly=True)
+
     user_by_email = get_user_by_email(email=user.email, db=db)
     if not Hasher.verify_password(user.password, user_by_email.hashed_password):
         raise InvalidCredentialsException()
     return user_by_email
 
 
-@router.post("/logout", response_model=Message)
-def logout():
+@router.post(
+    "/logout",
+    response_model=Message,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": Message},
+    },
+)
+def logout(
+    _: Annotated[str, Depends(refresh_token_dependency)],
+):
     """Logs the user out.
 
     Returns:
-        (JSONResponse): The response signalling a correct logout.
+        response (JSONResponse): The response signalling a correct logout.
     """
-    return JSONResponse(status_code=200, content={"message": ExceptionMessages.SUCCESS})
+    response = JSONResponse(
+        status_code=200, content={"message": ExceptionMessages.SUCCESS}
+    )
+    response.delete_cookie("access_token")
+    response.delete_cookie("refresh_token")
+    return response
+
+
+@router.get(
+    "/current",
+    response_model=User,
+    responses={
+        status.HTTP_401_UNAUTHORIZED: {"model": Message},
+        status.HTTP_404_NOT_FOUND: {"model": Message},
+    },
+)
+def get_current_user(
+    response: Response,
+    new_access_token: Annotated[str, Depends(refresh_token_dependency)],
+    current_user: Annotated[str, Depends(get_user_from_token)],
+):
+    """Gets current user from authentication cookies.
+
+    Args:
+        response (Response): Response object.
+        new_access_token (Annotated[str, Depends]): New JWT access token,
+            generated if the previous one has expired.
+            Defaults to Depends(refresh_token_dependency).
+        current_user (Annotated[str, Depends]): Current user read from access token,
+            Defaults to Depends(get_user_from_token).
+
+    Returns:
+        current_user (User): The currently logged in user.
+    """
+    if new_access_token:
+        response.set_cookie(key="access_token", value=new_access_token, httponly=True)
+    return current_user
