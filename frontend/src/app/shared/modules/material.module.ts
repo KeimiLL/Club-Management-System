67cd5be6712a5dc498eb0581c6513616e25b6138
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-08-04 18:01:58 +0200

    CMS-42: add mat card to material module

--- frontend/src/app/shared/modules/material.module.ts
+++ frontend/src/app/shared/modules/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from "@angular/core";
 import { MatButtonModule } from "@angular/material/button";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
-    exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+    exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
 })
 export class MaterialModule {}
