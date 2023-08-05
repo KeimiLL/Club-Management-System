62ab0408684494086978d54a11c868bfcd33a789
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-08-05 17:27:23 +0200

    CMS-43: prepare half-card-component

--- frontend/src/app/shared/components/half-card/half-card.component.ts
+++ frontend/src/app/shared/components/half-card/half-card.component.ts
@@ -1,11 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, EventEmitter, Input, Output } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { MaterialModule } from "../../modules/material.module";
 
 @Component({
     selector: "app-half-card",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MaterialModule],
     templateUrl: "./half-card.component.html",
     styleUrls: ["./half-card.component.scss"],
 })
-export class HalfCardComponent {}
+export class HalfCardComponent {
+    @Input() public isRight = true;
+    @Input() public name: string;
+    @Input() public subName = "";
+    @Output() public closedClicked: EventEmitter<void> = new EventEmitter();
+
+    public onClosedButtonClick(): void {
+        this.closedClicked.emit();
+    }
+}
