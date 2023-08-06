333b08b3651d1493e04bdbe7a55c4b942e652de7
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-08-06 21:01:05 +0200

    CMS-39: Add user interfaces that reflect backend schemas

--- frontend/src/app/shared/models/user.model.ts
+++ frontend/src/app/shared/models/user.model.ts
@@ -1,5 +1,33 @@
-export interface UserCreate {
+export interface UserBase {
     full_name: string;
     email: string;
+}
+
+export interface UserLogin {
+    email: string;
     password: string;
+}
+
+export interface UserCreate extends UserBase, UserLogin {}
+
+export interface UserCreateWithRole extends UserCreate {
+    role: Roles;
+}
+
+export interface User extends UserBase {
+    role: Roles;
+}
+
+export interface UserUpdate {
+    full_name?: string;
+    email?: string;
+    password?: string;
+    role?: Roles;
+}
+
+export enum Roles {
+    Admin = "admin",
+    Coach = "coach",
+    Player = "player",
+    Viewer = "viewer",
 }
