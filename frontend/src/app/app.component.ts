48ee9054f8c9ad5f0ba7e7c7dbacd8a079bedf61
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-08-06 20:57:47 +0200

    CMS-39: Remove unused test service

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,26 +1,8 @@
-import { Component, OnInit } from "@angular/core";
-import { TestService } from "./shared/services/test.service";
-import { UserCreate } from "./shared/models/user.model";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "app-root",
     templateUrl: "./app.component.html",
     styleUrls: ["./app.component.scss"],
 })
-export class AppComponent implements OnInit {
-    title = "CMS";
-
-    constructor(private readonly testService: TestService) {}
-
-    ngOnInit(): void {
-        const user: UserCreate = {
-            full_name: "testowy",
-            email: "testowy@gmail.com",
-            password: "abcABC1!",
-        };
-
-        this.testService.register(user).subscribe((data) => {
-            console.log(data);
-        });
-    }
-}
+export class AppComponent {}
