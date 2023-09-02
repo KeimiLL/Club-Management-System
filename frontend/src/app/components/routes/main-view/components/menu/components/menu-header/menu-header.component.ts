2b269bb5a4fdc69754448e7a50ae14d38933ea93
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-09-02 20:05:32 +0200

    CMS-67: change icon in menu

--- frontend/src/app/components/routes/main-view/components/menu/components/menu-header/menu-header.component.ts
+++ frontend/src/app/components/routes/main-view/components/menu/components/menu-header/menu-header.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { Component, EventEmitter, Input, Output } from "@angular/core";
+import { User } from "src/app/shared/models/user.model";
+import { UserService } from "src/app/shared/services/user.service";
 
 import { MaterialModule } from "../../../../../../../shared/modules/material.module";
 
@@ -11,8 +13,15 @@ import { MaterialModule } from "../../../../../../../shared/modules/material.mod
     imports: [CommonModule, MaterialModule],
 })
 export class MenuHeaderComponent {
+    public user: User;
     @Input() public isCollapsed: boolean;
     @Output() public collapsedClicked = new EventEmitter<void>();
+
+    constructor(private readonly userService: UserService) {
+        if (userService.currentUser !== null) {
+            this.user = userService.currentUser;
+        }
+    }
 
     public onCollapseButtonClick(): void {
         this.collapsedClicked.emit();
