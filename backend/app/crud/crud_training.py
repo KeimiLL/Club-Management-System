07bf852f3b4bd30887b42f4a1f00407e03ef778f
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-09-05 22:20:35 +0200

    CMS-62: Add crud operations for trainings table

---
+++ backend/app/crud/crud_training.py
@@ -0,0 +1,62 @@
+"""File responsible for implementing traininges related CRUD operations."""
+
+
+from app.core.exceptions import DuplicateException, MissingException
+from app.crud.crud_team import get_team_by_id
+from app.models.training import Training
+from app.schemas.training import TrainingCreate, TrainingInDBBase
+from sqlalchemy.exc import IntegrityError, NoResultFound, SQLAlchemyError
+from sqlalchemy.orm import Session
+
+
+def create_new_training(training: TrainingCreate, db: Session) -> Training:
+    """Creates a new training based on training data.
+
+    Args:
+        training (TrainingCreate): Training based on Training schema.
+        db (Session): Database session.
+
+    Raises:
+        DuplicateException: If there is already a training with the given id.
+        SQLAlchemyError: If there is a different exception.
+
+    Returns:
+        new_training (Training): Training object.
+    """
+    try:
+        get_team_by_id(training.team_id, db)
+        new_training = Training(
+            team_id=training.team_id,
+            notes=training.notes,
+            date=training.date,
+        )
+        db.add(new_training)
+        db.commit()
+        db.refresh(new_training)
+        return new_training
+    except IntegrityError as exc:
+        raise DuplicateException(Training.__name__) from exc
+    except SQLAlchemyError as exc:
+        raise exc
+
+
+def get_training_by_id(training_id: int, db: Session) -> TrainingInDBBase:
+    """Gets the training based on the given training id.
+
+    Args:
+        training_id (int): Training id.
+        db (Session): Database session.
+
+    Raises:
+        MissingException: If no training matches the given training id.
+        SQLAlchemyError: If there is a different exception.
+
+    Returns:
+        TrainingInDB: Training object.
+    """
+    try:
+        return db.query(Training).filter(Training.id == training_id).one()
+    except NoResultFound as exc:
+        raise MissingException(Training.__name__) from exc
+    except SQLAlchemyError as exc:
+        raise exc
