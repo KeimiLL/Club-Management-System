4229fa616470a8c615735bcb1575e2cbe33beadf
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-09-06 20:51:09 +0200

    CMS-62: Add crud operations for meetings table

---
+++ backend/app/crud/crud_meeting.py
@@ -0,0 +1,63 @@
+"""File responsible for implementing meetinges related CRUD operations."""
+
+
+from app.core.exceptions import DuplicateException, MissingException
+from app.crud.crud_user import get_user_by_id
+from app.models.meeting import Meeting
+from app.schemas.meeting import MeetingCreate, MeetingInDBBase
+from sqlalchemy.exc import IntegrityError, NoResultFound, SQLAlchemyError
+from sqlalchemy.orm import Session
+
+
+def create_new_meeting(meeting: MeetingCreate, db: Session) -> Meeting:
+    """Creates a new meeting based on meeting data.
+
+    Args:
+        meeting (MeetingCreate): Meeting based on Meeting schema.
+        db (Session): Database session.
+
+    Raises:
+        DuplicateException: If there is already a meeting with the given id.
+        SQLAlchemyError: If there is a different exception.
+
+    Returns:
+        new_meeting (Meeting): Meeting object.
+    """
+    try:
+        get_user_by_id(meeting.user_id, db)
+        new_meeting = Meeting(
+            user_id=meeting.user_id,
+            description=meeting.description,
+            notes=meeting.notes,
+            date=meeting.date,
+        )
+        db.add(new_meeting)
+        db.commit()
+        db.refresh(new_meeting)
+        return new_meeting
+    except IntegrityError as exc:
+        raise DuplicateException(Meeting.__name__) from exc
+    except SQLAlchemyError as exc:
+        raise exc
+
+
+def get_meeting_by_id(meeting_id: int, db: Session) -> MeetingInDBBase:
+    """Gets the meeting based on the given meeting id.
+
+    Args:
+        meeting_id (int): Meeting id.
+        db (Session): Database session.
+
+    Raises:
+        MissingException: If no meeting matches the given meeting id.
+        SQLAlchemyError: If there is a different exception.
+
+    Returns:
+        MeetingInDB: Meeting object.
+    """
+    try:
+        return db.query(Meeting).filter(Meeting.id == meeting_id).one()
+    except NoResultFound as exc:
+        raise MissingException(Meeting.__name__) from exc
+    except SQLAlchemyError as exc:
+        raise exc
