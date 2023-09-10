97cb3b81f0008784a60b6107010c394cbf2c1dd0
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-09-10 21:39:07 +0200

    CMS-76: Add the missing injury model import

--- backend/app/db/base.py
+++ backend/app/db/base.py
@@ -4,6 +4,7 @@
 # pylint: disable=unused-import
 from app.db.base_class import Base
 from app.models.coach import Coach
+from app.models.injury import Injury
 from app.models.match import Match
 from app.models.match_player import MatchPlayer
 from app.models.meeting import Meeting
