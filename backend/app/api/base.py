f4e94f7794b9e63b7b44ca9d297b883d6fa93992
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-09-14 22:32:47 +0200

    CMS-76: Create an endpoint for creating a single meeting

--- backend/app/api/base.py
+++ backend/app/api/base.py
@@ -1,8 +1,9 @@
 """Base file for including all routers."""
 
 
-from app.api.v1.endpoints import users
+from app.api.v1.endpoints import meetings, users
 from fastapi import APIRouter
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(users.router, prefix="/users", tags=["users"])
+api_router.include_router(meetings.router, prefix="/meetings", tags=["meetings"])
