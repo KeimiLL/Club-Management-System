dffae00f884776c2bd2b3eb2894463fdcd6052a5
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-09-23 22:50:02 +0200

    CMS-88: Create a schema for a list of items with their total number

--- backend/app/schemas/misc.py
+++ backend/app/schemas/misc.py
@@ -4,7 +4,7 @@
 from typing import NewType
 
 from app.schemas.enums import HTTPResponseMessage
-from pydantic import BaseModel, conint, conset
+from pydantic import BaseModel, NonNegativeInt, conint, conset
 
 
 class Message(BaseModel):
@@ -17,6 +17,13 @@ class MessageFromEnum(BaseModel):
     """Class representation of JSONResponse content message from HTTPResponseMessage enum."""
 
     message: HTTPResponseMessage
+
+
+class ItemsListWithTotal(BaseModel):
+    """Class for returning a list of items alongside the total number of them."""
+
+    items: list
+    total: NonNegativeInt
 
 
 DBIndexInt = NewType("DBIndexInt", conint(ge=1, lt=10**7))
