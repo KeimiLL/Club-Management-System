b3cc1f5e3515f00638cf3b35b510f3ddb7621da6
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-09-29 23:52:27 +0200

    CMS-89: Create an endpoint for getting single meeting data

--- backend/app/api/v1/endpoints/meetings.py
+++ backend/app/api/v1/endpoints/meetings.py
@@ -4,7 +4,12 @@
 from typing import Annotated
 
 from app.api.dependencies import get_user_from_token, paginate
-from app.crud.crud_meeting import get_all_meetings, get_meetings_by_user_id
+from app.core.exceptions import ForbiddenException
+from app.crud.crud_meeting import (
+    get_all_meetings,
+    get_meeting_by_id,
+    get_meetings_by_user_id,
+)
 from app.crud.crud_meeting_user import create_meeting_user_from_user_id_list
 from app.db.session import get_db
 from app.models.user import User
@@ -12,7 +17,7 @@ from app.schemas.enums import Roles
 from app.schemas.meeting import MeetingCreate, MeetingOnlyBaseUserInfo, MeetingTableView
 from app.schemas.meeting_user import MeetingUserCreateUserIdList
 from app.schemas.misc import ItemsListWithTotal, Message, MessageFromEnum
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Path, status
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -89,7 +94,7 @@ def get_meetings(
                     user_name=meeting.created_by_user.full_name,
                     is_yours=(
                         current_user.id
-                        in (meeting.user_id, *map(lambda u: u.id, meeting.users))
+                        in (meeting.user_id, *[user.id for user in meeting.users])
                     )
                 )
                 for meeting in meetings
@@ -111,3 +116,42 @@ def get_meetings(
         ],
         total=total,
     )
+
+
+@router.get(
+    "/{meeting_id}",
+    response_model=MeetingOnlyBaseUserInfo,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"model": Message},
+        status.HTTP_401_UNAUTHORIZED: {"model": Message},
+        status.HTTP_403_FORBIDDEN: {"model": Message},
+        status.HTTP_404_NOT_FOUND: {"model": Message},
+        status.HTTP_409_CONFLICT: {"model": MessageFromEnum},
+    },
+)
+def get_meeting(
+    meeting_id: Annotated[int, Path(ge=1, le=10**7)],
+    current_user: Annotated[User, Depends(get_user_from_token)],
+    db: Annotated[Session, Depends(get_db)],
+):
+    """Returns the meeting by the given id.
+
+    Args:
+        meeting_id (Annotated[int, Path]): The requested meeting's id. Has to be greater than
+            or equal to 1 and less than or equal to 10**7.
+        current_user (Annotated[User, Depends]): Current user read from access token.
+            Defaults to Depends(get_user_from_token).
+        db (Annotated[Session, Depends]): Database session. Defaults to Depends(get_db).
+
+     Raises:
+        ForbiddenException: If the current user does not have the sufficient permission.
+
+    Returns:
+        MeetingInDBOnlyBaseUserInfo: The requested meeting.
+    """
+    meeting = get_meeting_by_id(meeting_id=meeting_id, db=db)
+    if current_user.role in (Roles.ADMIN, Roles.BOARD):
+        return meeting
+    if current_user.id in (meeting.user_id, *[user.id for user in meeting.users]):
+        return meeting
+    raise ForbiddenException("meeting")
