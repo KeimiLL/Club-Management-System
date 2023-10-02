a1d453a9d6e7db61ab9081661fcb0d1e65b94533
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-02 22:44:57 +0200

    CMS-46: Add settings menu component tests

--- frontend/src/app/components/routes/main-view/components/settings/components/settings-menu/settings-menu.component.spec.ts
+++ frontend/src/app/components/routes/main-view/components/settings/components/settings-menu/settings-menu.component.spec.ts
@@ -1,21 +1,17 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-
+import { UserService } from "./../../../../../../../shared/services/user.service";
 import { SettingsMenuComponent } from "./settings-menu.component";
 
 describe("SettingsMenuComponent", () => {
     let component: SettingsMenuComponent;
-    let fixture: ComponentFixture<SettingsMenuComponent>;
+    let userService: jasmine.SpyObj<UserService>;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [SettingsMenuComponent],
-        });
-        fixture = TestBed.createComponent(SettingsMenuComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        userService = jasmine.createSpyObj("UserService", ["get currentUser"]);
+
+        component = new SettingsMenuComponent(userService);
     });
 
-    it("should create", () => {
+    it("should create the component", () => {
         expect(component).toBeTruthy();
     });
 });
