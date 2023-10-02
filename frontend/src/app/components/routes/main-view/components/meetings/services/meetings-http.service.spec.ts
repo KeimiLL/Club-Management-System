0801124565b9959a296f4f9ddb379fa2338ec57d
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-02 21:58:21 +0200

    CMS-46: Add meetings http service tests

--- frontend/src/app/components/routes/main-view/components/meetings/services/meetings-http.service.spec.ts
+++ frontend/src/app/components/routes/main-view/components/meetings/services/meetings-http.service.spec.ts
@@ -1,16 +1,20 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 
 import { MeetingsHttpService } from "./meetings-http.service";
 
 describe("MeetingsHttpService", () => {
-    let service: MeetingsHttpService;
+    let mockService: MeetingsHttpService;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
-        service = TestBed.inject(MeetingsHttpService);
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [MeetingsHttpService],
+        });
+        mockService = TestBed.inject(MeetingsHttpService);
     });
 
     it("should be created", () => {
-        expect(service).toBeTruthy();
+        expect(mockService).toBeTruthy();
     });
 });
