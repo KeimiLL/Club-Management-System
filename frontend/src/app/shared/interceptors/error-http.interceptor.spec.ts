8a6ebbf7aa48cda7518c5ef8b6865da0c9c94275
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-02 23:02:24 +0200

    CMS-46: Add error http interceptor tests

--- frontend/src/app/shared/interceptors/error-http.interceptor.spec.ts
+++ frontend/src/app/shared/interceptors/error-http.interceptor.spec.ts
@@ -1,17 +1,34 @@
 import { TestBed } from "@angular/core/testing";
+import { ActivatedRoute, Router } from "@angular/router";
 
+import { SnackbarService } from "../services/snackbar.service";
 import { ErrorHttpInterceptor } from "./error-http.interceptor";
 
 describe("ErrorHttpInterceptor", () => {
-    beforeEach(() =>
+    let interceptor: ErrorHttpInterceptor;
+    let snackService: jasmine.SpyObj<SnackbarService>;
+    let router: jasmine.SpyObj<Router>;
+    let activatedRoute: ActivatedRoute;
+
+    beforeEach(() => {
+        snackService = jasmine.createSpyObj("SnackbarService", [
+            "showHttpErrorSnackBar",
+        ]);
+        router = jasmine.createSpyObj("Router", ["parseUrl"]);
+
         TestBed.configureTestingModule({
-            providers: [ErrorHttpInterceptor],
-        })
-    );
+            providers: [
+                ErrorHttpInterceptor,
+                { provide: SnackbarService, useValue: snackService },
+                { provide: Router, useValue: router },
+                { provide: ActivatedRoute, useValue: activatedRoute },
+            ],
+        });
+
+        interceptor = TestBed.inject(ErrorHttpInterceptor);
+    });
 
     it("should be created", () => {
-        const interceptor: ErrorHttpInterceptor =
-            TestBed.inject(ErrorHttpInterceptor);
         expect(interceptor).toBeTruthy();
     });
 });
