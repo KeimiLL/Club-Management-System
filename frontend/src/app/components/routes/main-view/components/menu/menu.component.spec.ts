24139dc8d21092e5b0881ca868c727d6dbb6fcb4
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-04 15:10:51 +0200

    CMS-46: Add menu component test draft

--- frontend/src/app/components/routes/main-view/components/menu/menu.component.spec.ts
+++ frontend/src/app/components/routes/main-view/components/menu/menu.component.spec.ts
@@ -1,21 +1,29 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
 
+import { UserService } from "../../../../../shared/services/user.service";
 import { MenuComponent } from "./menu.component";
 
 describe("MenuComponent", () => {
     let component: MenuComponent;
     let fixture: ComponentFixture<MenuComponent>;
 
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [],
+            imports: [HttpClientTestingModule, MatSnackBarModule],
+            providers: [UserService],
+        }).compileComponents();
+    });
+
     beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [MenuComponent],
-        });
         fixture = TestBed.createComponent(MenuComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    it("should create", () => {
+    it("should be created", () => {
         expect(component).toBeTruthy();
     });
 });
