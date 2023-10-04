8f468fad06dab8bf9cdba45a911e0f576a2d20aa
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-04 10:29:22 +0200

    CMS-46: Add meeting table component tests

--- frontend/src/app/components/routes/main-view/components/meetings/components/meeting-table/meeting-table.component.spec.ts
+++ frontend/src/app/components/routes/main-view/components/meetings/components/meeting-table/meeting-table.component.spec.ts
@@ -1,21 +1,41 @@
+import { CommonModule } from "@angular/common";
+import { HttpClientModule } from "@angular/common/http";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MatPaginator } from "@angular/material/paginator";
+import { ActivatedRoute } from "@angular/router";
+import { of } from "rxjs";
 
+import { MaterialModule } from "../../../../../../../shared/modules/material.module";
+import { SplitViewManagerService } from "../../../../../../../shared/services/split-view-manager.service";
 import { MeetingTableComponent } from "./meeting-table.component";
+
+class MockActivatedRoute {
+    queryParams = of({});
+}
 
 describe("MeetingTableComponent", () => {
     let component: MeetingTableComponent;
     let fixture: ComponentFixture<MeetingTableComponent>;
 
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [CommonModule, MaterialModule, HttpClientModule],
+            declarations: [],
+            providers: [
+                MatPaginator,
+                SplitViewManagerService,
+                { provide: ActivatedRoute, useClass: MockActivatedRoute },
+            ],
+        }).compileComponents();
+    });
+
     beforeEach(() => {
-        TestBed.configureTestingModule({
-            imports: [MeetingTableComponent],
-        });
         fixture = TestBed.createComponent(MeetingTableComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
     });
 
-    it("should create", () => {
+    it("should be created", () => {
         expect(component).toBeTruthy();
     });
 });
