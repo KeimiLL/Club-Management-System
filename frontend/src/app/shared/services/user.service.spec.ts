9d5bfa25eed90ba372c8d8a46b01b82ef78a6c93
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-04 12:11:13 +0200

    CMS-46: Add user service tests

--- frontend/src/app/shared/services/user.service.spec.ts
+++ frontend/src/app/shared/services/user.service.spec.ts
@@ -1,16 +1,53 @@
+import {
+    HttpClientTestingModule,
+    HttpTestingController,
+} from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
 
+import { BackendResponse } from "../models/misc.model";
+import { UserCreate } from "../models/user.model";
 import { UserService } from "./user.service";
 
 describe("UserService", () => {
     let service: UserService;
+    let httpMock: HttpTestingController;
 
     beforeEach(() => {
-        TestBed.configureTestingModule({});
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [UserService],
+        });
         service = TestBed.inject(UserService);
+        httpMock = TestBed.inject(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpMock.verify();
     });
 
     it("should be created", () => {
         expect(service).toBeTruthy();
+    });
+
+    it("should register a user", () => {
+        const userCreate: UserCreate = {
+            full_name: "Janusz Tracz",
+            email: "janusz.tracz@plebania.com",
+            password: "12345678",
+        };
+
+        const mockResponse: BackendResponse = {
+            message: "User registered successfully",
+        };
+
+        service.register(userCreate).subscribe((response) => {
+            expect(response).toEqual(mockResponse);
+        });
+
+        const req = httpMock.expectOne("api/v1/users/register");
+        expect(req.request.method).toBe("POST");
+        expect(req.request.body).toEqual(userCreate);
+
+        req.flush(mockResponse);
     });
 });
