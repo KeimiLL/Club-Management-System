9a0ba124b4a0169b5629a4faa6168b4544d923b6
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-05 21:35:52 +0200

    CMS-46: Fix meeting table component test

--- frontend/src/app/components/routes/main-view/components/meetings/components/meeting-table/meeting-table.component.spec.ts
+++ frontend/src/app/components/routes/main-view/components/meetings/components/meeting-table/meeting-table.component.spec.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from "@angular/common";
 import { HttpClientModule } from "@angular/common/http";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { MatPaginator } from "@angular/material/paginator";
+import { MatPaginator, PageEvent } from "@angular/material/paginator";
 import { ActivatedRoute } from "@angular/router";
 import { of } from "rxjs";
 
 import { MaterialModule } from "../../../../../../../shared/modules/material.module";
 import { SplitViewManagerService } from "../../../../../../../shared/services/split-view-manager.service";
+import { TableService } from "../../../../../../../shared/services/table.service";
+import { MeetingsRootService } from "../../services/meetings-root.service";
 import { MeetingTableComponent } from "./meeting-table.component";
 
 class MockActivatedRoute {
@@ -16,6 +18,7 @@ class MockActivatedRoute {
 describe("MeetingTableComponent", () => {
     let component: MeetingTableComponent;
     let fixture: ComponentFixture<MeetingTableComponent>;
+    const mockEvent: PageEvent = { pageIndex: 0, pageSize: 2, length: 2 };
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
@@ -24,7 +27,22 @@ describe("MeetingTableComponent", () => {
             providers: [
                 MatPaginator,
                 SplitViewManagerService,
+                {
+                    provide: MeetingsRootService,
+                    useValue: {
+                        initData: () => null,
+                        displayedColumns$: of([]),
+                    },
+                },
                 { provide: ActivatedRoute, useClass: MockActivatedRoute },
+                {
+                    provide: TableService,
+                    useValue: {
+                        changePage: () => of(mockEvent.pageIndex),
+                        capacity: 1,
+                        totalItems$: of(1),
+                    },
+                },
             ],
         }).compileComponents();
     });
