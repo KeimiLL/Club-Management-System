81b5f739f9b78a04c4b16745e7a38b07f9c90b86
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-05 21:37:30 +0200

    CMS-46: Add meeting root service test

--- frontend/src/app/components/routes/main-view/components/meetings/services/meetings-root.service.spec.ts
+++ frontend/src/app/components/routes/main-view/components/meetings/services/meetings-root.service.spec.ts
@@ -1,16 +1,42 @@
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { TestBed } from "@angular/core/testing";
+import { MatDialogModule } from "@angular/material/dialog";
+import { of } from "rxjs";
 
+import { LongMeeting } from "../../../../../../shared/models/meetings.model";
+import { SplitViewManagerService } from "../../../../../../shared/services/split-view-manager.service";
+import { TableService } from "../../../../../../shared/services/table.service";
 import { MeetingsHttpService } from "./meetings-http.service";
 import { MeetingsRootService } from "./meetings-root.service";
+
+const mockLongMeeting: LongMeeting[] = [];
 
 describe("MeetingsRootService", () => {
     let mockService: MeetingsRootService;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [HttpClientTestingModule],
-            providers: [MeetingsRootService, MeetingsHttpService],
+            imports: [HttpClientTestingModule, MatDialogModule],
+            providers: [
+                MeetingsRootService,
+                MeetingsHttpService,
+                {
+                    provide: TableService,
+                    useValue: {
+                        getCurrentPage: () => of(mockLongMeeting),
+                        currentPageIndex$: of(1),
+                        currentPageIndex: 1,
+                        capacity: 1,
+                    },
+                },
+                {
+                    provide: SplitViewManagerService,
+                    useValue: {
+                        currentId$: of(null),
+                        isDetail$: of(true),
+                    },
+                },
+            ],
         });
         mockService = TestBed.inject(MeetingsRootService);
     });
