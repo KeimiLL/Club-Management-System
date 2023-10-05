5d753dc33f259463cd256e005c594526fe0652d0
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-05 21:33:41 +0200

    CMS-46: Add add meeting popup component test

--- frontend/src/app/components/routes/main-view/components/meetings/components/add-meeting-popup/add-meeting-popup.component.spec.ts
+++ frontend/src/app/components/routes/main-view/components/meetings/components/add-meeting-popup/add-meeting-popup.component.spec.ts
@@ -1,21 +1,45 @@
-// import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { of } from "rxjs";
 
-// import { NewMeetingPopupComponent } from "./add-meeting-popup.component";
+import { Roles } from "../../../../../../../shared/models/user.model";
+import { UserService } from "../../../../../../../shared/services/user.service";
+import { AddMeetingPopupComponent } from "./add-meeting-popup.component";
 
-// describe("NewMeetingPopupComponent", () => {
-//     let component: NewMeetingPopupComponent;
-//     let fixture: ComponentFixture<NewMeetingPopupComponent>;
+describe("AddMeetingPopupComponent", () => {
+    let component: AddMeetingPopupComponent;
+    let fixture: ComponentFixture<AddMeetingPopupComponent>;
 
-//     beforeEach(() => {
-//         TestBed.configureTestingModule({
-//             imports: [NewMeetingPopupComponent],
-//         });
-//         fixture = TestBed.createComponent(NewMeetingPopupComponent);
-//         component = fixture.componentInstance;
-//         fixture.detectChanges();
-//     });
+    const userServiceMock = {
+        currentUser: {
+            full_name: "Janusz Tracz",
+            email: "janusz.tracz@plebania.com",
+            role: Roles.Admin,
+            id: 1,
+        },
+        getAllUsers: () => of(null),
+    };
 
-//     it("should create", () => {
-//         expect(component).toBeTruthy();
-//     });
-// });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [
+                HttpClientTestingModule,
+                AddMeetingPopupComponent,
+                MatSnackBarModule,
+            ],
+            providers: [
+                { provide: UserService, useValue: userServiceMock },
+                { provide: MAT_DIALOG_DATA, useValue: {} },
+                { provide: MatDialogRef, useValue: {} },
+            ],
+        });
+        fixture = TestBed.createComponent(AddMeetingPopupComponent);
+        component = fixture.componentInstance;
+    });
+
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
+});
