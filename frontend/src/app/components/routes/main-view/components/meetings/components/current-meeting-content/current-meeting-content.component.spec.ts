8241e769f6e1eb4e0a4fa565132786c07bb9bd7f
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-05 21:34:56 +0200

    CMS-46: Add current meeting content component test

--- frontend/src/app/components/routes/main-view/components/meetings/components/current-meeting-content/current-meeting-content.component.spec.ts
+++ frontend/src/app/components/routes/main-view/components/meetings/components/current-meeting-content/current-meeting-content.component.spec.ts
@@ -1,6 +1,26 @@
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
+import { Meeting } from "../../../../../../../shared/models/meetings.model";
+import {
+    Roles,
+    ShortUser,
+} from "../../../../../../../shared/models/user.model";
 import { CurrentMeetingContentComponent } from "./current-meeting-content.component";
+
+const mockShortUser: ShortUser = {
+    full_name: "Janusz Tracz",
+    id: 1,
+    role: Roles.Admin,
+};
+const mockMeeting: Meeting = {
+    notes: null,
+    name: "xyz",
+    date: "123",
+    id: 1,
+    created_by_user: mockShortUser,
+    users: [mockShortUser],
+};
 
 describe("CurrentMeetingContentComponent", () => {
     let component: CurrentMeetingContentComponent;
@@ -8,11 +28,11 @@ describe("CurrentMeetingContentComponent", () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [CurrentMeetingContentComponent],
+            imports: [HttpClientTestingModule, CurrentMeetingContentComponent],
         });
         fixture = TestBed.createComponent(CurrentMeetingContentComponent);
         component = fixture.componentInstance;
-        fixture.detectChanges();
+        component.meeting = mockMeeting;
     });
 
     it("should create", () => {
