f4674cbb01e6d52d01120c799ff33f2a1e63efb5
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-06 07:56:32 +0200

    CMS-92: Create preloader basic html structure
        pick 6234676 CMS-92: Create spinner preloader standalone component
        pick 67fa64a CMS-92: Create preloader basic html structure
        pick 663e4a2 CMS-92: Add preloader spinner component styles
        pick dfa5f76 CMS-92: Add fade and use spinner in wide card content

--- frontend/src/app/shared/components/spinner/spinner.component.ts
+++ frontend/src/app/shared/components/spinner/spinner.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
+import { MaterialModule } from "../../modules/material.module";
+
 @Component({
     selector: "app-spinner",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MaterialModule],
     templateUrl: "./spinner.component.html",
     styleUrls: ["./spinner.component.scss"],
 })
--- frontend/src/app/shared/modules/material.module.ts
+++ frontend/src/app/shared/modules/material.module.ts
@@ -12,6 +12,7 @@ import { MatInputModule } from "@angular/material/input";
 import { MatListModule } from "@angular/material/list";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatSelectModule } from "@angular/material/select";
 import { MatTableModule } from "@angular/material/table";
 import { MatToolbarModule } from "@angular/material/toolbar";
@@ -36,6 +37,7 @@ import { MatTooltipModule } from "@angular/material/tooltip";
         MatTooltipModule,
         MatMenuModule,
         MatListModule,
+        MatProgressSpinnerModule,
     ],
 })
 export class MaterialModule {}
