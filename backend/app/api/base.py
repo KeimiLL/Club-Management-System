6f8afe91def07fecd67325c2952119cf92144fb9
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-10-11 21:45:42 +0200

    CMS-106: Add an endpoint for creating a team

--- backend/app/api/base.py
+++ backend/app/api/base.py
@@ -1,10 +1,11 @@
 """Base file for including all routers."""
 
 
-from app.api.v1.endpoints import coaches, meetings, users
+from app.api.v1.endpoints import coaches, meetings, teams, users
 from fastapi import APIRouter
 
 api_router = APIRouter(prefix="/api/v1")
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(meetings.router, prefix="/meetings", tags=["meetings"])
 api_router.include_router(coaches.router, prefix="/coaches", tags=["coaches"])
+api_router.include_router(teams.router, prefix="/teams", tags=["teams"])
---
+++ backend/app/api/v1/endpoints/teams.py
@@ -0,0 +1,56 @@
+"""Teams related endpoints."""
+
+
+from typing import Annotated
+
+from app.api.dependencies import get_user_from_token
+from app.core.exceptions import ForbiddenException
+from app.crud.crud_team import create_new_team
+from app.db.session import get_db
+from app.models.user import User
+from app.schemas.enums import HTTPResponseMessage, Roles
+from app.schemas.misc import Message, MessageFromEnum
+from app.schemas.team import TeamCreate
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+router = APIRouter()
+
+
+@router.post(
+    "",
+    response_model=Message,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"model": Message},
+        status.HTTP_401_UNAUTHORIZED: {"model": Message},
+        status.HTTP_403_FORBIDDEN: {"model": Message},
+        status.HTTP_404_NOT_FOUND: {"model": Message},
+        status.HTTP_409_CONFLICT: {"model": MessageFromEnum},
+    },
+)
+def create_team(
+    team: TeamCreate,
+    current_user: Annotated[User, Depends(get_user_from_token)],
+    db: Annotated[Session, Depends(get_db)],
+):
+    """Creates a new team based on data from a POST request.
+
+    Args:
+        team (TeamCreate): Team data from POST request.
+        current_user (Annotated[User, Depends]): Current user read from access token.
+            Defaults to Depends(get_user_from_token).
+        db (Annotated[Session, Depends]): Database session. Defaults to Depends(get_db).
+
+    Raises:
+        ForbiddenException: If the current user does not have sufficient permissions.
+
+    Returns:
+        Message: The response signalling a successful operation.
+    """
+    if current_user.role in (Roles.ADMIN, Roles.BOARD):
+        create_new_team(
+            team=team,
+            db=db,
+        )
+        return Message(message=HTTPResponseMessage.SUCCESS)
+    raise ForbiddenException("team")
