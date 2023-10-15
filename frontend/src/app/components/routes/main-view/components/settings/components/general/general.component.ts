83ff637df9c603ccf56a18b3d7fb4c26bd80ff1a
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-15 13:39:13 +0200

    CMS-101: Add basic template and styles for general component

--- frontend/src/app/components/routes/main-view/components/settings/components/general/general.component.ts
+++ frontend/src/app/components/routes/main-view/components/settings/components/general/general.component.ts
@@ -1,11 +1,25 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+
+import { CardsModule } from "../../../../../../../shared/modules/cards.module";
+import { MaterialModule } from "../../../../../../../shared/modules/material.module";
 
 @Component({
     selector: "app-general",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, CardsModule, MaterialModule],
     templateUrl: "./general.component.html",
     styleUrls: ["./general.component.scss"],
 })
-export class GeneralComponent {}
+export class GeneralComponent {
+    changePasswordForm: FormGroup;
+
+    saveChanges(): void {
+        if (this.changePasswordForm.valid) {
+            console.log("Changes saved");
+        } else {
+            console.log("Password fields are empty or invalid.");
+        }
+    }
+}
