1daeabc68f8b18174eb4c3b1c785cf3bb0cf9f46
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-10-17 18:53:55 +0200

    CMS-101: Create password form animation and add necessary function

--- frontend/src/app/components/routes/main-view/components/settings/components/general/general.component.ts
+++ frontend/src/app/components/routes/main-view/components/settings/components/general/general.component.ts
@@ -1,3 +1,10 @@
+import {
+    animate,
+    state,
+    style,
+    transition,
+    trigger,
+} from "@angular/animations";
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { FormGroup } from "@angular/forms";
@@ -11,15 +18,28 @@ import { MaterialModule } from "../../../../../../../shared/modules/material.mod
     imports: [CommonModule, CardsModule, MaterialModule],
     templateUrl: "./general.component.html",
     styleUrls: ["./general.component.scss"],
+    animations: [
+        trigger("formAnimation", [
+            state("hidden", style({ height: "0", opacity: 0 })),
+            state("visible", style({ height: "*", opacity: 1 })),
+            transition("hidden => visible", animate("0.5s ease-in-out")),
+            transition("visible => hidden", animate("0.5s ease-in-out")),
+        ]),
+    ],
 })
 export class GeneralComponent {
+    protected showForm: "hidden" | "visible" = "hidden";
     changePasswordForm: FormGroup;
 
-    saveChanges(): void {
+    protected saveChanges(): void {
         if (this.changePasswordForm.valid) {
             console.log("Changes saved");
         } else {
             console.log("Password fields are empty or invalid.");
         }
+    }
+
+    protected toggleForm(): void {
+        this.showForm = this.showForm === "hidden" ? "visible" : "hidden";
     }
 }
