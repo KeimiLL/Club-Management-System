2775dde0fdeadf383b99aa73b5470bbd776f060d
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-10-19 00:15:50 +0200

    CMS-130: filter by Control value pipe fix

--- frontend/src/app/shared/pipes/filter-using-prop-control.pipe.ts
+++ frontend/src/app/shared/pipes/filter-using-prop-control.pipe.ts
@@ -1,17 +1,20 @@
 import { Pipe, PipeTransform } from "@angular/core";
-import { AbstractControl } from "@angular/forms";
 
 @Pipe({
     name: "filterUsingPropControl",
     standalone: true,
 })
 export class FilterUsingPropControlPipe implements PipeTransform {
-    transform<T>(array: T[], property: keyof T, control: AbstractControl): T[] {
-        if (control.value === null) return array;
+    transform<T>(
+        array: T[],
+        property: keyof T,
+        controlValue: string | null
+    ): T[] {
+        if (controlValue === null) return array;
 
         return array.filter((item) => {
             const value = (item[property] as string).toLowerCase();
-            return value.includes(control.value.toLocaleLowerCase());
+            return value.includes(controlValue.toLocaleLowerCase());
         });
     }
 }
