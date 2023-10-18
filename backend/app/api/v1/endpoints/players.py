b1eb70b1d6346d5236356615506976c1309ae718
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-10-18 19:11:34 +0200

    CMS-109: Add an endpoint for getting players by team

--- backend/app/api/v1/endpoints/players.py
+++ backend/app/api/v1/endpoints/players.py
@@ -3,15 +3,19 @@
 
 from typing import Annotated
 
-from app.api.dependencies import get_user_from_token, refresh_token_dependency
+from app.api.dependencies import get_user_from_token, paginate, refresh_token_dependency
 from app.core.exceptions import ForbiddenException
-from app.crud.crud_player import create_new_player, get_all_players
+from app.crud.crud_player import (
+    create_new_player,
+    get_all_players,
+    get_players_with_pagination_by_team_id,
+)
 from app.db.session import get_db
 from app.models.user import User
 from app.schemas.enums import HTTPResponseMessage, Roles
-from app.schemas.misc import Message, MessageFromEnum
-from app.schemas.player import PlayerCreate, PlayerOnlyBaseInfo
-from fastapi import APIRouter, Depends, status
+from app.schemas.misc import ItemsListWithTotal, Message, MessageFromEnum
+from app.schemas.player import PlayerCreate, PlayerOnlyBaseInfo, PlayerTableView
+from fastapi import APIRouter, Depends, Query, status
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -80,3 +84,60 @@ def get_players(
         PlayerOnlyBaseInfo(**player.__dict__, user_full_name=player.user.full_name)
         for player in players
     ]
+
+
+@router.get(
+    "",
+    response_model=ItemsListWithTotal[PlayerTableView],
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"model": Message},
+        status.HTTP_401_UNAUTHORIZED: {"model": Message},
+        status.HTTP_403_FORBIDDEN: {"model": Message},
+        status.HTTP_404_NOT_FOUND: {"model": Message},
+        status.HTTP_409_CONFLICT: {"model": MessageFromEnum},
+    },
+)
+def get_players_by_team_id(
+    pagination: Annotated[dict[str, int], Depends(paginate)],
+    team_id: Annotated[int, Query(ge=1, lt=10000)],
+    current_user: Annotated[User, Depends(get_user_from_token)],
+    db: Annotated[Session, Depends(get_db)],
+):
+    """Gets the list of players that play in the team with the given id.
+
+    Args:
+        pagination (Annotated[dict[str, int], Depends]): Pagination read from the query params.
+            Defaults to Depends(paginate).
+        team_id (Annotated[int, Query]): The requested team's id. Has to be greater than
+            1 and less than or equal to 10**7.
+        current_user (Annotated[User, Depends]): Current user read from access token.
+            Defaults to Depends(get_user_from_token).
+        db (Annotated[Session, Depends]): Database session. Defaults to Depends(get_db).
+
+    Raises:
+        ForbiddenException: If the current user does not have sufficient permissions.
+
+    Returns:
+        ItemsListWithTotal[PlayerTableView]: A list of players alongside their total number.
+    """
+    if (
+        current_user.role in (Roles.ADMIN, Roles.BOARD)
+        or (
+            current_user.role == Roles.COACH
+            and team_id in [team.id for team in current_user.coach.teams]
+        )
+        or (
+            current_user.role == Roles.PLAYER and team_id == current_user.player.team_id
+        )
+    ):
+        players, total = get_players_with_pagination_by_team_id(
+            **pagination, team_id=team_id, db=db
+        )
+        return ItemsListWithTotal[PlayerTableView](
+            items=[
+                PlayerTableView(**player.__dict__, user_full_name=player.user.full_name)
+                for player in players
+            ],
+            total=total,
+        )
+    raise ForbiddenException("player")
