f89cadded66d4493ec5b0fcf0cd6980ea1ae6b1e
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-10-25 21:49:34 +0200

    CMS-145: Write tests for getting coach by team_id

--- backend/app/tests/test_crud/test_crud_coach.py
+++ backend/app/tests/test_crud/test_crud_coach.py
@@ -3,12 +3,20 @@
 
 import pytest
 from app.core.exceptions import MissingException
-from app.crud.crud_coach import create_new_coach, get_all_coaches, get_coach_by_user_id
+from app.crud.crud_coach import (
+    create_new_coach,
+    get_all_coaches,
+    get_coach_by_team_id,
+    get_coach_by_user_id,
+)
+from app.crud.crud_team import create_new_team
 from app.crud.crud_user import create_new_user
 from app.schemas.coach import CoachCreate
+from app.schemas.team import TeamCreate
 from app.schemas.user import UserCreate, UserCreateWithRole
 from app.tests.conftest import (
     coach_create,
+    team_create,
     user_create_unique_1,
     user_create_unique_2,
     user_create_with_role,
@@ -117,3 +125,40 @@ def test_correct__get_all_coaches(
             for coach, new_coach in zip(coaches, new_coaches)
         )
     )
+
+
+@pytest.mark.parametrize(
+    "user,coach,team,coach_id",
+    [
+        (user_create_unique_1, coach_create, team_create, 1),
+        (user_create_unique_1, coach_create, team_create, None),
+    ],
+)
+def test_correct__get_coach_by_team_id(
+    user: UserCreate,
+    coach: CoachCreate,
+    team: TeamCreate,
+    coach_id: int | None,
+    db_session: Session,
+) -> None:
+    """Tests getting a coach by team id.
+
+    Args:
+        user (UserCreate): User to be created.
+        coach (CoachCreate): Coach to be created.
+        team (TeamCreate): Team to be created.
+        coach_id (int | None): Coach id to be assigned to the team.
+        db_session (Session): Database session.
+    """
+    create_new_user(user, db_session)
+    new_coach = create_new_coach(coach, db_session)
+    new_team = create_new_team(
+        team.model_copy(update={"coach_id": coach_id}), db_session
+    )
+    coach_by_team_id = get_coach_by_team_id(new_team.id, db_session)
+    if coach_by_team_id is not None:
+        assert new_coach.user.full_name == coach_by_team_id.user.full_name
+        assert new_coach.date_of_birth == coach_by_team_id.date_of_birth
+        assert new_coach.date_of_joining == coach_by_team_id.date_of_joining
+    else:
+        assert coach_id is None
