82d2bdf1b63374b985e657d5b00242fbb46eca0c
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-10-25 21:52:35 +0200

    CMS-145: Create an endpoint for getting coach by team_id

--- backend/app/api/v1/endpoints/coaches.py
+++ backend/app/api/v1/endpoints/coaches.py
@@ -1,17 +1,19 @@
 """Coaches related endpoints."""
 
 
+from __future__ import annotations
+
 from typing import Annotated
 
 from app.api.dependencies import get_user_from_token, refresh_token_dependency
 from app.core.exceptions import ForbiddenException
-from app.crud.crud_coach import create_new_coach, get_all_coaches
+from app.crud import crud_coach
 from app.db.session import get_db
 from app.models.user import User
-from app.schemas.coach import CoachCreate, CoachOnlyBaseInfo
+from app.schemas.coach import CoachCreate, CoachOnlyBaseInfo, CoachOnlyName
 from app.schemas.enums import HTTPResponseMessage, Roles
 from app.schemas.misc import Message, MessageFromEnum
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Query, status
 from sqlalchemy.orm import Session
 
 router = APIRouter()
@@ -28,7 +30,7 @@ router = APIRouter()
         status.HTTP_409_CONFLICT: {"model": MessageFromEnum},
     },
 )
-def create_coach(
+def create_new_coach(
     coach: CoachCreate,
     current_user: Annotated[User, Depends(get_user_from_token)],
     db: Annotated[Session, Depends(get_db)],
@@ -48,7 +50,7 @@ def create_coach(
         Message: The response signalling a successful operation.
     """
     if current_user.role == Roles.ADMIN:
-        create_new_coach(
+        crud_coach.create_new_coach(
             coach=coach,
             db=db,
         )
@@ -63,7 +65,7 @@ def create_coach(
         status.HTTP_401_UNAUTHORIZED: {"model": Message},
     },
 )
-def get_coaches(
+def get_all_coaches(
     _: Annotated[str, Depends(refresh_token_dependency)],
     db: Annotated[Session, Depends(get_db)],
 ):
@@ -75,8 +77,54 @@ def get_coaches(
     Returns:
         list[CoachOnlyBaseInfo]: The list of all coaches.
     """
-    coaches = get_all_coaches(db=db)
+    coaches = crud_coach.get_all_coaches(db=db)
     return [
         CoachOnlyBaseInfo(**coach.__dict__, user_full_name=coach.user.full_name)
         for coach in coaches
     ]
+
+
+@router.get(
+    "",
+    response_model=CoachOnlyName,
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"model": Message},
+        status.HTTP_401_UNAUTHORIZED: {"model": Message},
+        status.HTTP_403_FORBIDDEN: {"model": Message},
+        status.HTTP_404_NOT_FOUND: {"model": Message},
+        status.HTTP_409_CONFLICT: {"model": MessageFromEnum},
+    },
+)
+def get_coach_by_team_id(
+    team_id: Annotated[int, Query(ge=1, lt=10000)],
+    current_user: Annotated[User, Depends(get_user_from_token)],
+    db: Annotated[Session, Depends(get_db)],
+):
+    """Gets the coach that is assigned to the team with the given id.
+
+    Args:
+        team_id (Annotated[int, Query]): The given team's id. Has to be greater than
+            1 and less than or equal to 10**7.
+        current_user (Annotated[User, Depends]): Current user read from access token.
+            Defaults to Depends(get_user_from_token).
+        db (Annotated[Session, Depends]): Database session. Defaults to Depends(get_db).
+
+    Raises:
+        ForbiddenException: If the current user does not have sufficient permissions.
+
+    Returns:
+        CoachOnlyName: The team's coach.
+    """
+    if (
+        current_user.role in (Roles.ADMIN, Roles.BOARD)
+        or (
+            current_user.role == Roles.COACH
+            and team_id in [team.id for team in current_user.coach.teams]
+        )
+        or (
+            current_user.role == Roles.PLAYER and team_id == current_user.player.team_id
+        )
+    ):
+        coach = crud_coach.get_coach_by_team_id(team_id=team_id, db=db)
+        return CoachOnlyName(user_full_name=coach.user.full_name if coach else None)
+    raise ForbiddenException("coach")
