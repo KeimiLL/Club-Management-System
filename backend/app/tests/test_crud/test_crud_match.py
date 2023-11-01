fcb4abc188b564be604157f988914fdf71a3a164
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-11-01 22:45:34 +0100

    CMS-153: Write tests for updating match state

--- backend/app/tests/test_crud/test_crud_match.py
+++ backend/app/tests/test_crud/test_crud_match.py
@@ -8,11 +8,13 @@ from app.crud.crud_match import (
     create_match_with_player_ids,
     create_new_match,
     get_match_by_id,
+    update_match_state,
 )
 from app.crud.crud_player import create_new_player
 from app.crud.crud_team import create_new_team
 from app.crud.crud_user import create_new_user
 from app.schemas.coach import CoachCreate
+from app.schemas.enums import MatchEvent
 from app.schemas.match import MatchCreate
 from app.schemas.player import PlayerCreate
 from app.schemas.team import TeamCreate
@@ -206,3 +208,57 @@ def test_incorrect__create_match_with_user_ids(
     create_new_player(player, db_session)
     with pytest.raises(MissingException):
         create_match_with_player_ids(match, player_ids, db_session)
+
+
+@pytest.mark.parametrize(
+    "user,coach,team,match,match_events",
+    [
+        (user_create_unique_1, coach_create, team_create, match_create, []),
+        (
+            user_create_unique_1,
+            coach_create,
+            team_create,
+            match_create,
+            [MatchEvent.START],
+        ),
+        (
+            user_create_unique_1,
+            coach_create,
+            team_create,
+            match_create,
+            [MatchEvent.START, MatchEvent.END],
+        ),
+    ],
+)
+def test_correct__update_match_state(
+    user: UserCreate,
+    coach: CoachCreate,
+    team: TeamCreate,
+    match: MatchCreate,
+    match_events: list[MatchEvent],
+    db_session: Session,
+) -> None:
+    """Tests updating match state.
+
+    Args:
+        user (UserCreate): User to be created.
+        coach (CoachCreate): Coach to be created.
+        team (TeamCreate): Team to be created.
+        match (MatchCreate): Match to be created.
+        match_events: list[MatchEvent],
+        db_session (Session): Database session.
+    """
+    create_new_user(user, db_session)
+    create_new_coach(coach, db_session)
+    create_new_team(team, db_session)
+    new_match = create_new_match(match, db_session)
+    for match_event in match_events:
+        update_match_state(match_event, new_match.id, db_session)
+    match_by_id = get_match_by_id(new_match.id, db_session)
+    assert match_by_id.notes == match.notes
+    assert match_by_id.date == match.date
+    assert new_match.has_started is (MatchEvent.START in match_events)
+    assert new_match.has_ended is (MatchEvent.END in match_events)
+    if new_match.has_started:
+        assert new_match.goals_scored == 0
+        assert new_match.goals_conceded == 0
