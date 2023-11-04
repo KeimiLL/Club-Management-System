372f93305559cae360c21168a68a6502f984598d
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-11-04 21:07:56 +0100

    CMS-149: Add carousel and content container to guide

--- frontend/src/app/components/routes/main-view/components/settings/components/help/help.component.ts
+++ frontend/src/app/components/routes/main-view/components/settings/components/help/help.component.ts
@@ -14,6 +14,26 @@ import { Photo, photos } from "./help.data";
 })
 export class HelpComponent {
     protected photos: Photo[] = photos;
+    protected currentPhotoIndex = 0;
+    protected currentPhoto: Photo = this.photos[this.currentPhotoIndex];
     protected showWelcomeCard = true;
+    protected showCarousel = false;
     protected showFarewellCard = false;
+
+    nextPhoto(): void {
+        if (this.currentPhotoIndex < this.photos.length - 1) {
+            this.currentPhotoIndex++;
+            this.currentPhoto = this.photos[this.currentPhotoIndex];
+        } else {
+            this.showCarousel = false;
+            this.showFarewellCard = true;
+        }
+    }
+
+    previousPhoto(): void {
+        if (this.currentPhotoIndex > 0) {
+            this.currentPhotoIndex--;
+            this.currentPhoto = this.photos[this.currentPhotoIndex];
+        }
+    }
 }
