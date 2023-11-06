7b751c110c907aba5198faf4189ed4f04035bd6d
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-11-07 00:02:32 +0100

    CMS-150: Add two questions and answers to info section

--- frontend/src/app/components/routes/main-view/components/settings/components/info/info.data.ts
+++ frontend/src/app/components/routes/main-view/components/settings/components/info/info.data.ts
@@ -6,14 +6,14 @@ export interface Faq {
 
 export const faq: Faq[] = [
     {
-        question: "Question 1",
-        answer: "Answer 1",
-        icon: "fingerprint",
+        question: "What is CMS?",
+        answer: "CMS is an acronym for Club Management System - comprehensive system used by sport clubs to efficiently manage various aspects of their operations. This system is designed to monitor and streamline ongoing processes, enhance team potential, automate various club-related tasks, and facilitate the smooth flow of information within the organization. In essence, a Club Management System serves as a centralized platform that empowers clubs to optimize their activities, improve their overall performance, and provide a seamless means of communication and information sharing among members and stakeholders.",
+        icon: "language",
     },
     {
-        question: "Question 2",
-        answer: "Answer 2",
-        icon: "attach_file",
+        question: "Who we are?",
+        answer: "The authors of the CMS are Dominik Sigulski, Bartosz Rolnik and Kamil Giziński. We are three young and ambitious frontend developers from Poland. When creating this application, we tried to take care of every detail to satisfy even the most demanding users. Thank you for choosing CMS!",
+        icon: "diversity_3",
     },
     {
         question: "Question 3",
