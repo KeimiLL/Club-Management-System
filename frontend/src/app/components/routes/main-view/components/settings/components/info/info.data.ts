8f35a1197e795f7ddf32b57cdaa04bba1914f3a1
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-11-06 23:26:15 +0100

    CMS-150: Add info data, interface and colors

---
+++ frontend/src/app/components/routes/main-view/components/settings/components/info/info.data.ts
@@ -0,0 +1,53 @@
+export interface Faq {
+    question: string;
+    answer: string;
+    icon: string;
+}
+
+export const faq: Faq[] = [
+    {
+        question: "Question 1",
+        answer: "Answer 1",
+        icon: "fingerprint",
+    },
+    {
+        question: "Question 2",
+        answer: "Answer 2",
+        icon: "attach_file",
+    },
+    {
+        question: "Question 3",
+        answer: "Answer 3",
+        icon: "cloud_queue",
+    },
+    {
+        question: "Question 4",
+        answer: "Answer 4",
+        icon: "timeline",
+    },
+    {
+        question: "Question 5",
+        answer: "Answer 5",
+        icon: "verified_user",
+    },
+    {
+        question: "Question 6",
+        answer: "Answer 6",
+        icon: "location_on",
+    },
+    {
+        question: "Question 7",
+        answer: "Answer 7",
+        icon: "weekend",
+    },
+    {
+        question: "Question 8",
+        answer: "Answer 8",
+        icon: "landscape",
+    },
+    {
+        question: "Question 9",
+        answer: "Answer 9",
+        icon: "local_phone",
+    },
+];
