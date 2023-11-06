bad63743162f0cc4fb79d50b8362bf7a55e304a1
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-11-06 23:29:49 +0100

    CMS-150: Create basic info structure

--- frontend/src/app/components/routes/main-view/components/settings/components/info/info.component.ts
+++ frontend/src/app/components/routes/main-view/components/settings/components/info/info.component.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
+import { CardsModule } from "../../../../../../../shared/modules/cards.module";
+import { MaterialModule } from "../../../../../../../shared/modules/material.module";
+
 @Component({
     selector: "app-info",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, CardsModule, MaterialModule],
     templateUrl: "./info.component.html",
     styleUrls: ["./info.component.scss"],
 })
