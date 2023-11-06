2338b28e04c25f9354cc218e102a62f39bdfcfba
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-11-06 23:34:07 +0100

    CMS-150: Add info section functionality

--- frontend/src/app/components/routes/main-view/components/settings/components/info/info.component.ts
+++ frontend/src/app/components/routes/main-view/components/settings/components/info/info.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 import { CardsModule } from "../../../../../../../shared/modules/cards.module";
 import { MaterialModule } from "../../../../../../../shared/modules/material.module";
+import { Faq, faq } from "./info.data";
 
 @Component({
     selector: "app-info",
@@ -11,4 +12,15 @@ import { MaterialModule } from "../../../../../../../shared/modules/material.mod
     templateUrl: "./info.component.html",
     styleUrls: ["./info.component.scss"],
 })
-export class InfoComponent {}
+export class InfoComponent implements OnInit {
+    protected faq: Faq[] = faq;
+    protected selectedFaq: Faq | null = null;
+
+    ngOnInit(): void {
+        [this.selectedFaq] = faq;
+    }
+
+    selectFaq(singleFaq: Faq): void {
+        this.selectedFaq = singleFaq;
+    }
+}
