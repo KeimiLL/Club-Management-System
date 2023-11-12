ac013257f0794579a59bad999736314193aabe53
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-11-12 19:02:41 +0100

    CMS-149: Add and use helpState getter and setter

--- frontend/src/app/components/routes/main-view/components/settings/components/help/help-root.service.ts
+++ frontend/src/app/components/routes/main-view/components/settings/components/help/help-root.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
 
 import { HelpState, Photo, photos } from "./help.data";
 
@@ -9,39 +9,49 @@ import { HelpState, Photo, photos } from "./help.data";
 export class HelpRootService {
     private currentPhotoIndex = 0;
     private readonly photos: Photo[] = photos;
-    private readonly helpStateSubject = new BehaviorSubject<HelpState>(
+    private readonly helpStateSubject$ = new BehaviorSubject<HelpState>(
         HelpState.Start
     );
 
-    helpState$ = this.helpStateSubject.asObservable();
+    private set helpState(helpState: HelpState) {
+        this.helpStateSubject$.next(helpState);
+    }
+
+    public get helpState(): HelpState {
+        return this.helpStateSubject$.value;
+    }
+
+    public get helpState$(): Observable<HelpState> {
+        return this.helpStateSubject$.asObservable();
+    }
 
     public startGuide(): void {
-        this.helpStateSubject.next(HelpState.Guide);
+        this.helpState = HelpState.Guide;
     }
 
     public restartGuide(): void {
         this.currentPhotoIndex = 0;
-        this.helpStateSubject.next(HelpState.Guide);
+        this.helpState = HelpState.Guide;
     }
 
     public nextPhoto(): void {
         if (this.currentPhotoIndex < this.photos.length - 1) {
             this.currentPhotoIndex++;
-            this.helpStateSubject.next(HelpState.Guide);
+            this.helpState = HelpState.Guide;
         } else {
-            this.helpStateSubject.next(HelpState.Finish);
+            this.helpState = HelpState.Finish;
         }
     }
 
     public previousPhoto(): void {
         if (this.currentPhotoIndex > 0) {
             this.currentPhotoIndex--;
-            this.helpStateSubject.next(HelpState.Guide);
+            this.helpState = HelpState.Guide;
         }
     }
 
     public toggleFarewellCard(): void {
-        this.helpStateSubject.next(HelpState.Finish);
+        this.helpState = HelpState.Finish;
     }
 
     public getPhotos(): Photo[] {
