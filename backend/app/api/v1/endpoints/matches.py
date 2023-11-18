e3ef26d604a05e4649c655025fa8722f479ebd60
Dominik Sigulski <58365481+Domin8668@users.noreply.github.com>
2023-11-18 13:29:06 +0100

    CMS-164: Create an endpoint for getting matches by team id

--- backend/app/api/v1/endpoints/matches.py
+++ backend/app/api/v1/endpoints/matches.py
@@ -4,20 +4,22 @@
 from typing import Annotated
 
 from app.api import player_not_allowed, viewer_not_allowed
-from app.core.exceptions import GenericException
+from app.api.dependencies import paginate
+from app.core.exceptions import ForbiddenException, GenericException
 from app.crud import crud_match
 from app.db.session import get_db
 from app.models.user import User
-from app.schemas.enums import HTTPResponseMessage, MatchEvent
+from app.schemas.enums import HTTPResponseMessage, MatchEvent, Roles
 from app.schemas.match import (
     MatchBase,
     MatchCreate,
     MatchInProgress,
     MatchScore,
     MatchScoreUpdate,
+    MatchTableView,
 )
 from app.schemas.match_player import MatchPlayerCreatePlayerIdList
-from app.schemas.misc import Message, MessageFromEnum
+from app.schemas.misc import ItemsListWithTotal, Message, MessageFromEnum
 from fastapi import APIRouter, Depends, Path, Query, status
 from sqlalchemy.orm import Session
 
@@ -173,3 +175,60 @@ def get_matches_in_progress_with_limit(
         MatchInProgress(**match.__dict__, team_name=match.team.name)
         for match in matches
     ]
+
+
+@router.get(
+    "",
+    response_model=ItemsListWithTotal[MatchTableView],
+    responses={
+        status.HTTP_400_BAD_REQUEST: {"model": Message},
+        status.HTTP_401_UNAUTHORIZED: {"model": Message},
+        status.HTTP_403_FORBIDDEN: {"model": MessageFromEnum},
+        status.HTTP_404_NOT_FOUND: {"model": Message},
+        status.HTTP_409_CONFLICT: {"model": MessageFromEnum},
+    },
+)
+def get_matches_with_pagination_by_team_id(
+    pagination: Annotated[dict[str, int], Depends(paginate)],
+    team_id: Annotated[int, Query(ge=1, lt=10000)],
+    current_user: Annotated[User, Depends(viewer_not_allowed)],
+    db: Annotated[Session, Depends(get_db)],
+):
+    """Gets the list of matches played by the team with the given id.
+
+    Args:
+        pagination (Annotated[dict[str, int], Depends]): Pagination read from the query params.
+            Defaults to Depends(paginate).
+        team_id (Annotated[int, Query]): The given team's id. Has to be greater than
+            1 and less than or equal to 10**7.
+        current_user (Annotated[User, Depends]): Current user read from access token.
+            Defaults to Depends(viewer_not_allowed).
+        db (Annotated[Session, Depends]): Database session. Defaults to Depends(get_db).
+
+    Raises:
+        ForbiddenException: If the current user does not have sufficient permissions.
+
+    Returns:
+        ItemsListWithTotal[MatchTableView]: A list of matches alongside their total number.
+    """
+    if (
+        current_user.role in (Roles.ADMIN, Roles.BOARD)
+        or (
+            current_user.role == Roles.COACH
+            and team_id in [team.id for team in current_user.coach.teams]
+        )
+        or (
+            current_user.role == Roles.PLAYER and team_id == current_user.player.team_id
+        )
+    ):
+        matches, total = crud_match.get_matches_with_pagination_by_team_id(
+            **pagination, team_id=team_id, db=db
+        )
+        return ItemsListWithTotal[MatchTableView](
+            items=[
+                MatchTableView(**match.__dict__, team_name=match.team.name)
+                for match in matches
+            ],
+            total=total,
+        )
+    raise ForbiddenException()
