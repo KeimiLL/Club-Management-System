d6683840cbdd9a40ab7985f0a0147549bf083213
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-11-24 20:27:48 +0100

    CMS-166: hook match

--- frontend/src/app/components/routes/main-view/components/schedule/components/schedule-content/schedule-content.component.ts
+++ frontend/src/app/components/routes/main-view/components/schedule/components/schedule-content/schedule-content.component.ts
@@ -2,14 +2,17 @@ import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
 import { Observable } from "rxjs";
 
+import { SpinnerComponent } from "../../../../../../../shared/components/spinner/spinner.component";
 import {
     Match,
     MatchContentType,
+    MatchScore,
 } from "../../../../../../../shared/models/match.model";
 import { CardsModule } from "../../../../../../../shared/modules/cards.module";
 import { MaterialModule } from "../../../../../../../shared/modules/material.module";
 import { SplitViewManagerService } from "../../../../../../../shared/services/split-view-manager.service";
 import { ScheduleContentService } from "../../services/schedule-content.service";
+import { MatchDetails } from "../schedule-popup/newMatchFormBuilder";
 import { MatchDetailsComponent } from "./components/match-details/match-details.component";
 import { MatchEventsComponent } from "./components/match-events/match-events.component";
 import { MatchSquadComponent } from "./components/match-squad/match-squad.component";
@@ -26,15 +29,24 @@ import { ScoreComponent } from "./components/score/score.component";
         MatchSquadComponent,
         MatchDetailsComponent,
         ScoreComponent,
+        SpinnerComponent,
     ],
     templateUrl: "./schedule-content.component.html",
     styleUrls: ["./schedule-content.component.scss"],
 })
 export class ScheduleContentComponent {
-    @Input() public match: Match;
+    @Input() public set match(match: Match) {
+        this.matchScore = { ...match };
+        this.matchSquad = match.players.map((player) => player.user_full_name);
+        this.matchBase = { ...match };
+    }
 
-    // protected isCurrentMeetingLoading$: Observable<boolean>;
-    // protected spinnerMessage = "Loading selected match...";
+    protected matchScore: MatchScore;
+    protected matchSquad: string[];
+    protected matchBase: MatchDetails;
+
+    protected isCurrentMatchLoading$: Observable<boolean>;
+    protected spinnerMessage = "Loading selected match...";
 
     protected readonly contentTypes = MatchContentType;
     protected readonly contentType$: Observable<MatchContentType>;
@@ -44,6 +56,7 @@ export class ScheduleContentComponent {
         private readonly split: SplitViewManagerService<Match>
     ) {
         this.contentType$ = this.content.contentType$;
+        this.isCurrentMatchLoading$ = this.split.isLoading$;
     }
 
     protected toggleContentType(contentType: MatchContentType): void {
