00c7d6ebf7de693980a2b4ed9025ac9580a54e43
KeimiLL <72435036+KeimiLL@users.noreply.github.com>
2023-11-27 22:50:23 +0100

    CMS-151: Add basic schedule structure

--- frontend/src/app/components/routes/main-view/components/schedule/components/schedule-content/components/match-events/match-events.component.ts
+++ frontend/src/app/components/routes/main-view/components/schedule/components/schedule-content/components/match-events/match-events.component.ts
@@ -1,11 +1,28 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+
+import {
+    MatchEvent,
+    MatchEventType,
+} from "../../../../../../../../../shared/models/match-event.model";
+
+const dumbEvent: MatchEvent = {
+    minute: 10,
+    event_type: MatchEventType.Goal,
+    description: "Tu jest opis zdarzenia",
+    is_own_event: true,
+};
 
 @Component({
     selector: "app-match-events",
     standalone: true,
-    imports: [CommonModule],
+    imports: [CommonModule, MatIconModule],
     templateUrl: "./match-events.component.html",
     styleUrls: ["./match-events.component.scss"],
 })
-export class MatchEventsComponent {}
+export class MatchEventsComponent {
+    MatchEventType = MatchEventType;
+    dumbEvent = dumbEvent;
+    @Input() public event: MatchEvent;
+}
