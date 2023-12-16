967000b66d48ceacd967bdc76dcb5e3da7f6e6ff
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-12-16 14:58:46 +0100

    CMS-182: add live matches componnets styles

--- frontend/src/app/components/routes/main-view/components/dashboard/components/live-matches/components/live-match/live-match.component.ts
+++ frontend/src/app/components/routes/main-view/components/dashboard/components/live-matches/components/live-match/live-match.component.ts
@@ -1,25 +1,35 @@
 import { CommonModule } from "@angular/common";
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { Observable } from "rxjs";
 
 import { MatchEventsContainerComponent } from "../../../../../../../../../shared/components/match-events-container/match-events-container.component";
 import { ScoreComponent } from "../../../../../../../../../shared/components/score/score.component";
 import { LiveMatch } from "../../../../../../../../../shared/models/match.model";
 import { MatchEvent } from "../../../../../../../../../shared/models/match-event.model";
+import { MaterialModule } from "../../../../../../../../../shared/modules/material.module";
 import { LiveMatchesRootService } from "../../services/live-matches-root.service";
 
 @Component({
     selector: "app-live-match[match]",
     standalone: true,
-    imports: [CommonModule, ScoreComponent, MatchEventsContainerComponent],
+    imports: [
+        CommonModule,
+        ScoreComponent,
+        MatchEventsContainerComponent,
+        MaterialModule,
+    ],
     templateUrl: "./live-match.component.html",
     styleUrls: ["./live-match.component.scss"],
 })
-export class LiveMatchComponent {
+export class LiveMatchComponent implements OnInit {
     @Input() public match: LiveMatch;
     protected events$: Observable<MatchEvent[]>;
 
     constructor(private readonly root: LiveMatchesRootService) {}
+
+    ngOnInit(): void {
+        this.fetchEvents$();
+    }
 
     public fetchEvents$(): void {
         this.events$ = this.root.getMatchEventsByMatchId$(this.match.id);
