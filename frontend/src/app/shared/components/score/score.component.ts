095255ef66f155e929e564bf24d1ee812b5e0985
rolero4 <72434958+Rolero4@users.noreply.github.com>
2023-12-16 15:12:37 +0100

    CMS-182: secure score with not having too long sentance on dashboard naming of teams

--- frontend/src/app/shared/components/score/score.component.ts
+++ frontend/src/app/shared/components/score/score.component.ts
@@ -3,14 +3,16 @@ import { Component, Input } from "@angular/core";
 
 import { LiveMatch, Match } from "../../models/match.model";
 import { ScorePipe } from "../../pipes/score.pipe";
+import { TooLongSentencePipe } from "../../pipes/too-long-sentence.pipe";
 
 @Component({
-    selector: "app-score",
+    selector: "app-score[match]",
     standalone: true,
-    imports: [CommonModule, ScorePipe],
+    imports: [CommonModule, ScorePipe, TooLongSentencePipe],
     templateUrl: "./score.component.html",
     styleUrls: ["./score.component.scss"],
 })
 export class ScoreComponent {
     @Input() public match: LiveMatch | Match;
+    @Input() public maxChars?: number;
 }
